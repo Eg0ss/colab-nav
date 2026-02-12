6b35fbb7da2b8966c1ced00bb48c618977521ffb
Marcel <agueroyessia@gmail.com>
2026-02-12 16:59:52 +0100

    feature/navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </header>
 </template>
 
 <style scoped></style>
